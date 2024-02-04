33a11f23460fa7b70fa1ebca693be9ec5106e279
YASH SHARMA <yashshar.2323@gmail.com>
2024-02-04 07:44:32 +0530

    Frontend: setup to fetch API and other ui advancements

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -1,23 +1,50 @@
-import React from "react";
-import CardComponent from  "./CardComponent"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CardComponent from './CardComponent';
 
-interface Card {
-    id: number;
-    name: string;
-    email:string;
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
 
 interface UserInterfaceProps {
-    backendName: string;
+  backendName: string;
 }
 
-const UserInterface: React.FC<UserInterfaceProps> = ({backendName}) => {
-    return(
-        <div>
-            <h1>{backendName}</h1>
-            <CardComponent card={{id:1, name: 'John Doe', email:'mail'}} />
-        </div>
-    );
-}
+const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+  const [users, setUsers] = useState<User[]>([]);
+  const [newUser, setNewUser] = useState({ name: '', email: '' });
+  const [updateUser, setUpdateUser] = useState({ id: '', name: '', email: '' });
+
+  const backgroundColors: { [key: string]: string } = {
+    flask: 'bg-blue-500',
+  };
+
+  const buttonColors: { [key: string]: string } = {
+    flask: 'bg-blue-700 hover:bg-blue-600',
+  };
+
+  const bgColor = backgroundColors[backendName as keyof typeof backgroundColors] || 'bg-gray-200';
+  const btnColor = buttonColors[backendName as keyof typeof buttonColors] || 'bg-gray-500 hover:bg-gray-600';
+
+  return (
+    <div className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
+      <img src={`/${backendName}logo.svg`} alt={`${backendName} Logo`} className="w-20 h-20 mb-6 mx-auto" />
+      <h2 className="text-xl font-bold text-center text-white mb-6">{`${backendName.charAt(0).toUpperCase() + backendName.slice(1)} Backend`}</h2>
+
+      {/* Display users */}
+      <div className="space-y-4">
+        {users.map((user) => (
+          <div key={user.id} className="flex items-center justify-between bg-white p-4 rounded-lg shadow">
+            <CardComponent card={user} />
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default UserInterface;
